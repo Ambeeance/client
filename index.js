00954dc76569bcfad06516c0c127effdc001cbbf
Elliot Iddon <elliot@citrusbytes.net>
2021-02-19 16:37:46 +0000

    Add websocket based Ambeeance client
    This is quite rough around the edges, could definitely use some error
    handling/disconnect recovery/etc. but you can give it an object and
    its flat keys will be sent to Ambeeance and autospec'd.
    
    Changes to values in Ambeeance will be assigned to the object if it
    has a matching key.

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-const ambeeance = () => {};
+const WebSocket = require('ws');
+
+const enabled = process.env.AMBEEANCE === 'ENABLED';
+const host = process.env.AMBEEANCE_HOST || 'localhost';
+const port = process.env.AMBEEANCE_PORT || 8335;
+
+let client;
+let clientPromise;
+if (enabled) {
+  const url = `ws://${host}:${port}`;
+  client = new WebSocket(url);
+  clientPromise = new Promise((resolve) => {
+    client.once('open', () => resolve(client));
+  });
+}
+const ambeeance = (config) => {
+  const configProxy = config;
+  if (enabled) {
+    client.on('message', (msg) => {
+      const configs = JSON.parse(msg);
+      Object.keys(configProxy).forEach((key) => {
+        if (typeof configs[key] !== 'undefined') {
+          configProxy[key] = configs[key];
+        }
+      });
+    });
+    clientPromise.then((connectedClient) => {
+      connectedClient.send(
+        JSON.stringify({
+          type: 'AUTOSPEC',
+          args: Object.keys(configProxy).map((key) =>
+            ({
+              key,
+              value: configProxy[key],
+            }.filter(
+              (spec) =>
+                ['string', 'boolean', 'number'].indexOf(
+                  typeof spec.value,
+                ) > 0,
+            )),
+          ),
+        }),
+      );
+    });
+  }
+  return configProxy;
+};
 
 module.exports = {
   ambeeance,
